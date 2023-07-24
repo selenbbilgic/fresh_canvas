ba95e033e49eae5ab368dbac530e2bdcdaf54f5b
selen bilgic <selen96@hotmail.com>
2023-07-24 16:07:53 +0300

    aa

--- index.jsx
+++ index.jsx
@@ -127,5 +127,35 @@ canvas.addEventListener("wheel", (event) => {
     // update zoom value
     zoom = newZoom;
   }
-});
 
+  // Left and Right Arrow Code
+const leftMenu = document.querySelector(".leftMenuContainer");
+const rightMenu = document.querySelector(".rightMenuContainer");
+
+const leftArrowBtn = document.querySelector("#leftBtn");
+const rightArrowBtn = document.querySelector("#rightBtn");
+
+leftArrowBtn.addEventListener("click", function () {
+  leftMenu.classList.toggle("active");
+    document.addEventListener("click", function (e) {
+      if (
+        !e.composedPath().includes(leftArrowBtn) &&
+        !e.composedPath().includes(leftMenu)
+      ) {
+        leftMenu.classList.remove("active");
+      }
+    });
+  });
+
+  rightArrowBtn.addEventListener("click", function () {
+    rightMenu.classList.toggle("active");
+    document.addEventListener("click", function (e) {
+      if (
+        !e.composedPath().includes(rightArrowBtn) &&
+        !e.composedPath().includes(rightMenu)
+      ) {
+        rightMenu.classList.remove("active");
+      }
+    });
+  });
+});
