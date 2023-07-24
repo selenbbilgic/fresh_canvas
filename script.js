e7d10b6fc334e4d58df0bfdca865c769439e5786
Kaan Dai <79655072+KDAI19@users.noreply.github.com>
2023-07-24 16:10:36 +0300

    aa

---
+++ script.js
@@ -0,0 +1,33 @@
+// Left and Right Arrow Code
+const leftMenu = document.querySelector(".leftMenuContainer");
+const rightMenu = document.querySelector(".rightMenuContainer");
+
+const leftArrowBtn = document.querySelector("#leftBtn");
+const rightArrowBtn = document.querySelector("#rightBtn");
+
+leftArrowBtn.addEventListener("click", function () {
+  leftMenu.classList.toggle("active");
+    document.addEventListener("click", function (e) {
+      if (
+        !e.composedPath().includes(leftArrowBtn) &&
+        !e.composedPath().includes(leftMenu)
+      ) {
+        leftMenu.classList.remove("active");
+      }
+    });
+  });
+
+  rightArrowBtn.addEventListener("click", function () {
+    rightMenu.classList.toggle("active");
+    document.addEventListener("click", function (e) {
+      if (
+        !e.composedPath().includes(rightArrowBtn) &&
+        !e.composedPath().includes(rightMenu)
+      ) {
+        rightMenu.classList.remove("active");
+      }
+    });
+  });
+
+
+  
