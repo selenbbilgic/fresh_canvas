62241f71afef3f1711ede485324ea7373b2e47cd
selen bilgic <selen96@hotmail.com>
2023-07-24 17:51:37 +0300

    failed react girişiöi

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MyCanvasComponent from "./MyCanvasComponent";
+
+ReactDOM.render(<MyCanvasComponent />, document.getElementById("root"));
